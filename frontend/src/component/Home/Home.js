e2716f12ee554e8efa72a53445f57b278a609c29
atulattri <atulattri852@gmail.com>
2023-07-06 10:00:56 +0530

    initial commit

---
+++ frontend/src/component/Home/Home.js
@@ -0,0 +1,63 @@
+import React, { Fragment, useEffect } from "react";
+// import { CgMouse } from "react-icons/all";
+import "./Home.css";
+import ProductCard from "./ProductCard.js";
+import MetaData from "../layout/MetaData";
+import { clearErrors, getProduct } from "../../actions/productAction";
+import { useSelector, useDispatch } from "react-redux";
+import Loader from "../layout/Loader/Loader";
+import { useAlert } from "react-alert";
+import { Link } from "@material-ui/core";
+
+const Home = () => {
+  const alert = useAlert();
+  const dispatch = useDispatch();
+  const { loading, error, products } = useSelector((state) => state.products);
+  console.log(products);
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+    dispatch(getProduct());
+  }, [dispatch, error, alert]);
+
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title="ECOMMERCE" />
+
+          <div className="banner">
+            <p>Welcome to Ecommerce</p>
+            <h1>FIND AMAZING PRODUCTS BELOW</h1>
+
+            <a href="#container">
+              <button>
+                Scroll
+              </button>
+            </a>
+            <Link href="/search" >
+               <div className="ssearch" >
+                Search
+               </div>
+            </Link>
+          </div>
+
+          <h2 className="homeHeading">Featured Products</h2>
+
+          <div className="container" id="container">
+            {products &&
+              products.map((product) => (
+                <ProductCard key={product._id} product={product} />
+              ))}
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+export default Home;
---
+++ frontend/src/component/layout/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import playStore from "../../../images/playstore.png";
+import appStore from "../../../images/Appstore.png";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+      
+      <div className="midFooter">
+        <h1>ECOMMERCE</h1>
+        <p>High Quality and Cheap Products</p>
+
+        <p>Copyrights 2021 &copy; Atul Attri</p>
+      </div>
+
+      
+    </footer>
+  );
+};
+
+export default Footer;
